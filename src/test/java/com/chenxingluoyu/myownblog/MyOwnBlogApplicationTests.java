6fc97d0183090dbe7e6d5619d1f9c1108756d13c
CHY <1170439724@qq.com>
2023-06-28 17:41:44 +0800

    创建

---
+++ src/test/java/com/chenxingluoyu/myownblog/MyOwnBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenxingluoyu.myownblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
